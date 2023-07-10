315e0d2ee645c058c68fc24675a16373e7b29250
INYONG KIM <ohnyong87@gmail.com>
2023-07-10 13:18:45 +0900

    Commit 1
    연결 테스트용
    
    Flask Framework 임포트
    render_template, request, jsonify 임포트
    
    1.메인 뷰페이지 반환
    2.POST방식 요청에 대한 처리
    3.GET방식 요청에 대한 처리
    4.app 서버 실행 5001포트

---
+++ app.py
@@ -0,0 +1,30 @@
+# 라이브러리 임포트
+# Flask Framework
+# view페이지 렌더링을 위한 render_template 메서드
+# 요청 데이터에 접근 할 수 있는 flask.request 모듈
+# dictionary를 json형식의 응답 데이터를 내보낼 수 있는 jsonify 메서드
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+# "localhost:5001/" URL요청에 메인 뷰 페이지 반환 응답
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# "localhost:5001/mars" URL POST방식 요청에 응답
+@app.route("/mars", methods=["POST"])
+def mars_post():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg':'POST 연결 완료!'})
+
+# "localhost:5001/mars" URL GET방식 요청에 응답
+@app.route("/mars", methods=["GET"])
+def mars_get():
+    return jsonify({'msg':'GET 연결 완료!'})
+
+# app이라는 메인 함수 
+# if __name__ == "__main__" 의 의미는 메인 함수의 선언, 시작을 의미
+# 이 파이썬 스크립트가 직접 실행될 때에는 main() 함수를 실행하라
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5001, debug=True)
